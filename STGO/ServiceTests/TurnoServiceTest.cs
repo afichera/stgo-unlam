32dbca2e04d3d346f1181ca217c81b88a91e801e
alejandrofichera@gmail.com <alejandrofichera@gmail.com@20fa6001-68ee-5bdc-a512-65041209ea96>
2012-11-11 22:14:57 +0000

    Se crea test, servicio de turno para obtencion de turnos libres.

---
+++ STGO/ServiceTests/TurnoServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.ServiceImpl;
+using Model;
+using System.Diagnostics;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class TurnoServiceTest
+    {
+        [TestMethod]
+        public void TestExtraerTurnosReservados()
+        {
+            List<Turno> turnosTotales = new List<Turno>();
+            List<Turno> turnosReservados = new List<Turno>();
+            int frecuencia = 15;
+            int cantidadTurnos = 20;
+            Turno turnoTotal1;
+            for (int i = 0; i < cantidadTurnos; i++)
+			{
+                turnoTotal1 = new Turno();
+                turnoTotal1.FechaHoraInicio = DateTime.Now.AddMinutes(frecuencia*(i+1));
+                turnoTotal1.FechaHoraFin = turnoTotal1.FechaHoraInicio.AddMinutes(frecuencia);
+                turnosTotales.Add(turnoTotal1);
+			}
+            Turno turnoReservado1 = new Turno();
+            turnoReservado1.FechaHoraInicio = DateTime.Now.AddMinutes(frecuencia * 2);
+            turnoReservado1.FechaHoraInicio = turnoReservado1.FechaHoraInicio.AddMinutes(frecuencia);
+
+            Turno turnoReservado2 = new Turno();
+            turnoReservado2.FechaHoraInicio = DateTime.Now.AddMinutes(frecuencia * 3);
+            turnoReservado2.FechaHoraInicio = turnoReservado1.FechaHoraInicio.AddMinutes(frecuencia);
+            
+            turnosReservados.Add(turnoReservado1);
+            turnosReservados.Add(turnoReservado2);
+
+            TurnoService turnoService = new TurnoService();//Voy testear metodo privado.
+            List<Turno> turnosLibres =  turnoService.extraerTurnosOcupados(turnosTotales, turnosReservados);
+            
+            Trace.WriteLine("-----Turnos Totales-----");
+            foreach (var turnoTotal in turnosTotales)
+            {
+                Trace.WriteLine("Fecha Hora Inicial: " + turnoTotal.FechaHoraInicio.ToLongTimeString() + ", Fecha Hora Final: " + turnoTotal.FechaHoraFin.ToLongTimeString());
+            }
+            Trace.WriteLine("Turnos Reservados");
+            foreach (var turnoReservado in turnosReservados)
+        	{
+                Trace.WriteLine("Fecha Hora Inicial: " + turnoReservado.FechaHoraInicio.ToLongTimeString() + ", Fecha Hora Final: " + turnoReservado.FechaHoraFin.ToLongTimeString());
+	        }
+
+            Trace.WriteLine("Turnos Libres");
+            foreach (var turnoLibre in turnosLibres)
+            {
+                Trace.WriteLine("Fecha Hora Inicial: " + turnoLibre.FechaHoraInicio.ToLongTimeString() + ", Fecha Hora Final: " + turnoLibre.FechaHoraFin.ToLongTimeString());
+            }
+            
+            Assert.IsTrue(turnosLibres.Count == 17);
+        }
+    }
+}
--- STGO/Services/ServiceImpl/TurnoService.cs
+++ STGO/Services/ServiceImpl/TurnoService.cs
@@ -12,6 +12,7 @@ namespace Services.ServiceImpl
     public class TurnoService:ITurnoService
     {
         private ITurnoDAO turnoDAO = DAOLocator.Instance.TurnoDAO;
+        private ISalaDAO salaDAO = DAOLocator.Instance.SalaDAO;
 
         public List<Turno> getAll()
         {
@@ -40,7 +41,81 @@ namespace Services.ServiceImpl
 
         public List<Turno> obtenerTurnosLibres(long idSala, DateTime fecha)
         {
-            throw new NotImplementedException();
+            Sala sala = this.salaDAO.getFindById(idSala);
+            List<Turno> turnosReservados = this.obtenerTurnosReservados(idSala, fecha);
+            List<Turno> turnosTotales = new List<Turno>();
+            List<Turno> turnosLibres;
+            Turno turno;
+            DateTime horarioInicioAux;
+            DateTime horarioFinAux;
+            int horaInicio = sala.HoraInicio.Hour;
+            int horaFin = sala.HoraCierre.Hour;
+            int minutosInicio = sala.HoraInicio.Minute;
+            int minutosFin = sala.HoraCierre.Minute;
+            int frecuencia = sala.Frecuencia;
+            
+            if((horaInicio*60+minutosInicio)>=(horaFin*60+minutosFin)){
+                //TODO: Lanzar Exception.
+            }
+            //Inicializo en el primer horario
+            horarioInicioAux = sala.HoraInicio;
+            horarioFinAux = sala.HoraInicio.AddMinutes(frecuencia);
+            int totalMinutosDia = (((horaFin * 60) + minutosFin) / ((horaInicio * 60) + minutosInicio));            
+            int maximoTurnosDia = totalMinutosDia / frecuencia;
+            for (int i = 0; i < maximoTurnosDia; i++)
+            {
+                turno = new Turno();
+                turno.FechaHoraInicio = horarioInicioAux;
+                turno.FechaHoraFin = horarioFinAux;
+                turno.Descripcion = "";
+                turno.Reservador = "";
+                
+                horarioFinAux = horarioFinAux.AddMinutes(frecuencia);
+                horarioInicioAux = horarioInicioAux.AddMinutes(frecuencia);
+                turnosTotales.Add(turno);
+            }
+            turnosLibres = this.extraerTurnosOcupados(turnosTotales, turnosReservados);
+            return turnosLibres;
+        }
+
+        public List<Turno> extraerTurnosOcupados(List<Turno> turnosTotales, List<Turno> turnosReservados){
+            List<Turno> turnosLibres = new List<Turno>();
+            int minutoInicialTurnoReservado;
+            int minutoFinalTurnoReservado;
+            int minutoInicialLibre;
+            int minutoFinalLibre;
+            bool agregarTurno;
+            if (turnosReservados != null && turnosReservados.Count > 0)
+            {
+                foreach (var turnoLibre in turnosTotales)
+                {
+                    agregarTurno = true;
+                    minutoInicialLibre = turnoLibre.FechaHoraInicio.Hour * 60 + turnoLibre.FechaHoraInicio.Minute;
+                    minutoFinalLibre = turnoLibre.FechaHoraFin.Hour * 60 + turnoLibre.FechaHoraFin.Minute;
+
+                    foreach (var turnoReservado in turnosReservados)
+                    {
+                        minutoInicialTurnoReservado = turnoReservado.FechaHoraInicio.Hour * 60 + turnoReservado.FechaHoraInicio.Minute;
+                        minutoFinalTurnoReservado = turnoReservado.FechaHoraFin.Hour * 60 + turnoReservado.FechaHoraFin.Minute;
+
+                        if ((minutoInicialTurnoReservado >= minutoInicialLibre && minutoInicialTurnoReservado <= minutoFinalLibre)
+                            || (minutoFinalTurnoReservado >= minutoInicialLibre && minutoFinalTurnoReservado <= minutoFinalLibre))
+                        {
+                            agregarTurno = false;
+                        }
+                        
+                    }
+
+                    if (agregarTurno) {
+                        turnosLibres.Add(turnoLibre);
+                    }
+                }
+            }
+            else {
+                turnosLibres = turnosTotales;
+            }
+
+            return turnosLibres;
         }
 
         public Turno obtenerTurno(long idSala, long idTurno)
