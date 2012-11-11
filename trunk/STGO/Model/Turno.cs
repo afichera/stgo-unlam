3f90e648cfaf698017c13e639b3a7583fbc6fa3a
alejandrofichera@gmail.com <alejandrofichera@gmail.com@20fa6001-68ee-5bdc-a512-65041209ea96>
2012-11-11 22:43:19 +0000

    Agrego Excepcion y Ordenamiento de Turnos Libres

--- trunk/STGO/Model/Turno.cs
+++ trunk/STGO/Model/Turno.cs
@@ -5,12 +5,26 @@ using System.Text;
 
 namespace Model
 {
-    public class Turno
+    public class Turno : IComparable<Turno>
     {
         public long Id { get; set; }
         public String Reservador { get; set; }
         public DateTime FechaHoraInicio { get; set; }
         public DateTime FechaHoraFin { get; set; }
         public String Descripcion { get; set; }
+
+
+
+        public int CompareTo(Turno other)
+        {
+            if (this.FechaHoraInicio == null)
+                return (other.FechaHoraInicio == null) ? 0 : 1;
+
+            if (other.FechaHoraInicio == null)
+                return -1;
+
+            return this.FechaHoraInicio.CompareTo(other.FechaHoraInicio);
+            
+        }
     }
 }
