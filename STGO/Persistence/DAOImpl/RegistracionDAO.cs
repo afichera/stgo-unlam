7383d825a252acf57e0182d826cb00b1a959ca59
alejandrofichera@gmail.com <alejandrofichera@gmail.com@20fa6001-68ee-5bdc-a512-65041209ea96>
2012-11-16 02:45:41 +0000

    Manejo de Excepciones y Logg de errores

--- STGO/Persistence/DAOImpl/RegistracionDAO.cs
+++ STGO/Persistence/DAOImpl/RegistracionDAO.cs
@@ -7,11 +7,14 @@ using Model;
 using System.Data.SqlClient;
 using System.Data;
 using Model.Exceptions;
+using log4net;
+using Persistence.Util;
 
 namespace Persistence.DAOImpl
 {
-    public class RegistracionDAO:BaseDAO, IRegistracionDAO
+    public class RegistracionDAO : BaseDAO, IRegistracionDAO
     {
+        private static ILog logger = log4net.LogManager.GetLogger(typeof(RegistracionDAO));
         public List<Registracion> getAll()
         {
             throw new NotImplementedException();
@@ -35,125 +38,188 @@ namespace Persistence.DAOImpl
         public Registracion getFindByUserName(string userName)
         {
             Registracion registracion = null;
-            if (base.Conectar())
+            try
             {
-                SqlDataReader dataReader;
-                base.Command = new SqlCommand();
-
-                base.Command.Connection = base.Conexion;
-                Command.CommandText = "SELECT r.razonSocial, r.email, r.cuit, r.telefono, r.fechaHoraRegistro, r.pendiente, r.link FROM Registracion r WHERE r.email = @userName";
-                Command.CommandType = CommandType.Text;
-                Command.Parameters.AddWithValue("userName", userName);
-                dataReader = Command.ExecuteReader();
-
-                while (dataReader.Read())
+                if (base.Conectar())
                 {
-                    registracion = new Registracion();
-                    registracion.RazonSocial =dataReader.GetSqlString(0).ToString();
-                    registracion.Email = dataReader.GetSqlString(1).ToString();
-                    registracion.Cuit = dataReader.GetSqlString(2).ToString();
-                    registracion.Telefono = dataReader.GetSqlString(3).ToString();                    
-                    registracion.FechaHoraRegistro = DateTime.Parse(dataReader.GetSqlDateTime(4).ToString());
-                    registracion.Pendiente = Convert.ToBoolean(dataReader.GetSqlByte(5).Value);
-                    registracion.linkActivacion = new Guid(dataReader.GetSqlGuid(6).ToString());
+                    SqlDataReader dataReader;
+                    base.Command = new SqlCommand();
+
+                    base.Command.Connection = base.Conexion;
+                    Command.CommandText = "SELECT r.razonSocial, r.email, r.cuit, r.telefono, r.fechaHoraRegistro, r.pendiente, r.link FROM Registracion r WHERE r.email = @userName";
+                    Command.CommandType = CommandType.Text;
+                    Command.Parameters.AddWithValue("userName", userName);
+                    dataReader = Command.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        registracion = new Registracion();
+                        registracion.RazonSocial = dataReader.GetSqlString(0).ToString();
+                        registracion.Email = dataReader.GetSqlString(1).ToString();
+                        registracion.Cuit = dataReader.GetSqlString(2).ToString();
+                        registracion.Telefono = dataReader.GetSqlString(3).ToString();
+                        registracion.FechaHoraRegistro = DateTime.Parse(dataReader.GetSqlDateTime(4).ToString());
+                        registracion.Pendiente = Convert.ToBoolean(dataReader.GetSqlByte(5).Value);
+                        registracion.linkActivacion = new Guid(dataReader.GetSqlGuid(6).ToString());
+                    }
                 }
+                else
+                {
+                    logger.Error(Constantes.ERROR_BDD_CONEXION);
+                    throw new BDDException();
+                }
+                return registracion;
+            }
+            catch (SqlException ex)
+            {
+                logger.Error("Ocurrio un error al intentar obtener la Registración de la Base de datos. Detalle: " + ex.Message);
+                throw new BDDException("Ocurrio un error al intentar obtener la Registración de la Base de datos. Detalle: " + ex.Message);
+            }
+            finally
+            {
                 base.Desconectar();
             }
-            return registracion;
+
         }
 
 
         public void crearPendiente(Registracion entity, Guid guid)
         {
             Registracion registracion = entity;
-
-            if (base.Conectar())
+            try
             {
+                if (base.Conectar())
+                {
 
-                string sp = "SP_REGISTRACION_CREAR_PENDIENTE";
-                SqlCommand Command = new SqlCommand(sp, base.Conexion);
-                Command.CommandType = CommandType.StoredProcedure;
-                SqlParameter paramUserId = new SqlParameter("userId", SqlDbType.UniqueIdentifier);
-                SqlParameter paramTelefono = new SqlParameter("telefono", SqlDbType.VarChar);
-                SqlParameter paramCuit = new SqlParameter("cuit", SqlDbType.VarChar);
-                SqlParameter paramRazonSocial = new SqlParameter("razonSocial", SqlDbType.VarChar);
+                    string sp = "SP_REGISTRACION_CREAR_PENDIENTE";
+                    SqlCommand Command = new SqlCommand(sp, base.Conexion);
+                    Command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter paramUserId = new SqlParameter("userId", SqlDbType.UniqueIdentifier);
+                    SqlParameter paramTelefono = new SqlParameter("telefono", SqlDbType.VarChar);
+                    SqlParameter paramCuit = new SqlParameter("cuit", SqlDbType.VarChar);
+                    SqlParameter paramRazonSocial = new SqlParameter("razonSocial", SqlDbType.VarChar);
 
-                paramUserId.Direction = ParameterDirection.Input;
-                paramUserId.Value = guid;
+                    paramUserId.Direction = ParameterDirection.Input;
+                    paramUserId.Value = guid;
 
-                paramTelefono.Direction = ParameterDirection.Input;
-                paramTelefono.Value = registracion.Telefono;
+                    paramTelefono.Direction = ParameterDirection.Input;
+                    paramTelefono.Value = registracion.Telefono;
 
-                paramCuit.Direction = ParameterDirection.Input;
-                paramCuit.Value = registracion.Cuit;
+                    paramCuit.Direction = ParameterDirection.Input;
+                    paramCuit.Value = registracion.Cuit;
 
-                paramRazonSocial.Direction = ParameterDirection.Input;
-                paramRazonSocial.Value = registracion.RazonSocial;
+                    paramRazonSocial.Direction = ParameterDirection.Input;
+                    paramRazonSocial.Value = registracion.RazonSocial;
 
-                Command.Parameters.Add(paramUserId);
-                Command.Parameters.Add(paramTelefono);
-                Command.Parameters.Add(paramCuit);
-                Command.Parameters.Add(paramRazonSocial);
-                
-                int filasAfectadas = Command.ExecuteNonQuery();
+                    Command.Parameters.Add(paramUserId);
+                    Command.Parameters.Add(paramTelefono);
+                    Command.Parameters.Add(paramCuit);
+                    Command.Parameters.Add(paramRazonSocial);
+
+                    int filasAfectadas = Command.ExecuteNonQuery();
+
+                }
+                else
+                {
+                    logger.Error(Constantes.ERROR_BDD_CONEXION);
+                    throw new BDDException();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                logger.Error("Ocurrio un error al intentar crear la registración pendiente en la Base de Datos. Detalle: " + ex.Message);
+                throw new BDDException("Ocurrio un error al intentar crear la registración pendiente en la Base de Datos. Detalle: " + ex.Message);
+            }
+            finally
+            {
 
                 base.Desconectar();
             }
-            
+
         }
 
 
         public Registracion getFindByActivationKey(Guid activationKey)
         {
             Registracion registracion = null;
-            if (base.Conectar())
+            try
             {
-                SqlDataReader dataReader;
-                base.Command = new SqlCommand();
-
-                base.Command.Connection = base.Conexion;
-                Command.CommandText = "SELECT r.razonSocial, r.email, r.cuit, r.telefono, r.fechaHoraRegistro, r.pendiente, r.link FROM Registracion r WHERE r.link= @key";
-                Command.CommandType = CommandType.Text;
-                Command.Parameters.AddWithValue("key", activationKey);
-                dataReader = Command.ExecuteReader();
-
-                while (dataReader.Read())
+                if (base.Conectar())
                 {
-                    registracion = new Registracion();
-                    registracion.RazonSocial = dataReader.GetSqlString(0).ToString();
-                    registracion.Email = dataReader.GetSqlString(1).ToString();
-                    registracion.Cuit = dataReader.GetSqlString(2).ToString();
-                    registracion.Telefono = dataReader.GetSqlString(3).ToString();
-                    registracion.FechaHoraRegistro = DateTime.Parse(dataReader.GetSqlDateTime(4).ToString());
-                    registracion.Pendiente = Convert.ToBoolean(dataReader.GetSqlByte(5).Value);
-                    registracion.linkActivacion = new Guid(dataReader.GetSqlGuid(6).ToString());
+                    SqlDataReader dataReader;
+                    base.Command = new SqlCommand();
+
+                    base.Command.Connection = base.Conexion;
+                    Command.CommandText = "SELECT r.razonSocial, r.email, r.cuit, r.telefono, r.fechaHoraRegistro, r.pendiente, r.link FROM Registracion r WHERE r.link= @key";
+                    Command.CommandType = CommandType.Text;
+                    Command.Parameters.AddWithValue("key", activationKey);
+                    dataReader = Command.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        registracion = new Registracion();
+                        registracion.RazonSocial = dataReader.GetSqlString(0).ToString();
+                        registracion.Email = dataReader.GetSqlString(1).ToString();
+                        registracion.Cuit = dataReader.GetSqlString(2).ToString();
+                        registracion.Telefono = dataReader.GetSqlString(3).ToString();
+                        registracion.FechaHoraRegistro = DateTime.Parse(dataReader.GetSqlDateTime(4).ToString());
+                        registracion.Pendiente = Convert.ToBoolean(dataReader.GetSqlByte(5).Value);
+                        registracion.linkActivacion = new Guid(dataReader.GetSqlGuid(6).ToString());
+                    }
                 }
+                else
+                {
+                    logger.Error(Constantes.ERROR_BDD_CONEXION);
+                    throw new BDDException();
+                }
+                return registracion;
+            }
+            catch (SqlException ex)
+            {
+                logger.Error("Ocurrio un error al obtener la Registracion por clave de activación. Detalle: " + ex.Message);
+                throw new BDDException("Ocurrio un error al obtener la Registracion por clave de activación. Detalle: " + ex.Message);
+            }
+            finally
+            {
                 base.Desconectar();
             }
-            return registracion;
         }
 
         public void activarCuenta(Guid activationKey)
         {
-           if (base.Conectar())
+            try
             {
-
-                string sp = "SP_ACTIVAR_CUENTA";
-                SqlCommand Command = new SqlCommand(sp, base.Conexion);
-                Command.CommandType = CommandType.StoredProcedure;
-                SqlParameter paramKeyId = new SqlParameter("key", SqlDbType.UniqueIdentifier);
-                paramKeyId.Direction = ParameterDirection.Input;
-                paramKeyId.Value = activationKey;
-
-                Command.Parameters.Add(paramKeyId);
-
-                int filasAfectadas = Command.ExecuteNonQuery();
-                base.Desconectar();
-                if (filasAfectadas < 1)
+                if (base.Conectar())
                 {
-                    throw new BusinessException("Error al activar cuenta.");
+                    string sp = "SP_ACTIVAR_CUENTA";
+                    SqlCommand Command = new SqlCommand(sp, base.Conexion);
+                    Command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter paramKeyId = new SqlParameter("key", SqlDbType.UniqueIdentifier);
+                    paramKeyId.Direction = ParameterDirection.Input;
+                    paramKeyId.Value = activationKey;
+
+                    Command.Parameters.Add(paramKeyId);
+
+                    int filasAfectadas = Command.ExecuteNonQuery();
+                    base.Desconectar();
+                    if (filasAfectadas < 1)
+                    {
+                        logger.Error("Error al activar cuenta.");
+                        throw new BusinessException("Error al activar cuenta.");
+                    }
                 }
             }
+            catch (SqlException ex)
+            {
+                logger.Error("Ocurrio un error al intentar activar la cuenta"+ex.Message);
+                throw new BDDException("Ocurrio un error al intentar activar la cuenta" + ex.Message);
+            }
+            finally
+            {
+                base.Desconectar();
+            }
+
         }
     }
 }
